d35806ec3157a5f5cd745628d93b84f30e656bf1
timgall <119544130+timgall@users.noreply.github.com>
2023-03-31 14:26:55 -0500

    simplified

--- app.js
+++ app.js
@@ -5,26 +5,24 @@ let previousAnswers = []
 
 while(true){
 let input = prompt("Guess a number")
-let number = Number(input);    
+let number = Number(input);
+previousAnswers.push(" "+number)
     if(input===null){
         alert("Goodbye, quitter!");
         break
     }else if(!Number.isInteger(number)|| number===""){
         alert("Enter a integer")
         numTries+=1;
-        previousAnswers.push(" "+number)
     }else {
         if(number === secretNumber){
-             alert("Correct! It only took you "+numTries+ " guesses!  Your previous guesses were"+ previousAnswers+".")
+             alert("Correct! Your previous guesses were "+ previousAnswers+".")
              numTries+=1;
              break
         }else if(number<secretNumber){
             alert("Guess higher!")
-            previousAnswers.push(" "+number)
             numTries+=1;   
         }else{
             alert('Guess lower!')
-            previousAnswers.push(" "+number)
             numTries+=1;
         } 
     }
