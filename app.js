293631c4bde28e151de93da850057e8b082696e8
timgall <119544130+timgall@users.noreply.github.com>
2023-03-31 14:38:39 -0500

    updated with name feature

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 
 let secretNumber = Math.floor(Math.random() * 20)+1;
-let numTries = 0;
+// let numTries = 0;
 let previousAnswers = []
+let nameResponse=[]
+
+let name = prompt("What is your name?")
+nameResponse.push(name)
 
 while(true){
-let input = prompt("Guess a number")
+    let input = prompt("Guess a number")
 let number = Number(input);
 previousAnswers.push(" "+number)
     if(input===null){
@@ -12,18 +16,18 @@ previousAnswers.push(" "+number)
         break
     }else if(!Number.isInteger(number)|| number===""){
         alert("Enter a integer")
-        numTries+=1;
+        // numTries+=1;
     }else {
         if(number === secretNumber){
-             alert("Correct! Your previous guesses were "+ previousAnswers+".")
+             alert("Correct " + nameResponse + "! Your previous guesses were "+ previousAnswers+".")
              numTries+=1;
              break
         }else if(number<secretNumber){
-            alert("Guess higher!")
-            numTries+=1;   
+            alert("Sorry, "+nameResponse+" guess higher!")
+            // numTries+=1;   
         }else{
-            alert('Guess lower!')
-            numTries+=1;
+            alert("Sorry, "+nameResponse+"  lower!")
+            // numTries+=1;
         } 
     }
 }
