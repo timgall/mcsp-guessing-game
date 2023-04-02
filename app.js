bc82ac4c48ec3555d071358e2a90ac96f33d8815
timgall <119544130+timgall@users.noreply.github.com>
2023-04-02 13:23:14 -0500

    final

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 let gameHistory={};
 console.log(gameHistory)
-
 function play(){
     let name = prompt("What is your name?");
     let input = prompt("Guess a number");
-    let secretNumber = Math.floor(Math.random() * 11);
+    let secretNumber = Math.floor(Math.random() * 20);
     console.log(secretNumber);
     let numTries = 0;
     let previousAnswers=[];
-    // let number = Number(input);
     while(input!=secretNumber){
         if(input<secretNumber){
             console.log(`input: ${input}, secretNumber: ${secretNumber}`)
@@ -33,42 +31,13 @@ var finalCount = numTries;
 console.log(`name; ${name}, finalcount: ${finalCount}`)
 updateGameHistory(name,finalCount)
 replayFunction()
-    // if(number===secretNumber){               
-    //         for(var i = 0; i<=gameHistory.length; i++){
-    //             if(gameHistory[i]===nameResponse){
-    //                 for (var j=0;j<=gameHistory[i].length;j++){
-    //                     if(gameHistory[nameResponse[j]]>=numTries){
-    //                         var scoreMore = numTries-gameHistory[nameResponse[j]]; 
-    //                         alert ("That\’s Correct "+ nameResponse+ "! And you beat your previous attempt by "+ scoreMore+" fewer guesses!");
-    //                         break
-    //                     }
-    //                     else if(gameHistory[nameResponse[j]]<=numTries){
-    //                         var scoreLess=gameHistory[nameResponse[j]]-numTries;    
-    //                         alert("That\’s Correct "+ nameResponse+"! You did better in your last game by "+scoreLess+" fewer guesses.");
-    //                     break
-    //                     }
-    //                     else{       
-    //                         alert("Correct " + nameResponse + "! Your previous guesses were "+previousAnswers+".");
-    //                     break
-    //                     }
-    //                 }
-    //             }else{
-    //                     gameHistory.push(nameResponse[numTries]);
-    //                     alert ("That\'s Correct" + nameResponse + "! Your previous guesses we\'re " + previousAnswers + "!");
-    //                 }
-    //             gameHistory.push(gameHistory[nameResponse[numTries]])   
-    //             } 
-    //     replayFunction()
-    //     }
-    // }
 }
 function replayFunction(){
     let replayRequest = prompt("Would you like to play again?")
     if(replayRequest.toLowerCase()==="yes"||replayRequest.toLowerCase()==="y"){
         play()    
-        }
-    else{
-            alert("Goodbye " + nameResponse + "!")
+    }else{
+        alert("Goodbye " + nameResponse + "!")
     }
 }
 function updateGameHistory(name, finalCount){
@@ -76,22 +45,20 @@ function updateGameHistory(name, finalCount){
     if(!gameHistory[name]){
     gameHistory[name]=finalCount;
     console.log(`Game History: ${JSON.stringify(gameHistory)}`)//JSON.stringify converts it from the type to the literal string
-    }
-    else if(gameHistory[name]<=finalCount){
+    }else if(gameHistory[name]<=finalCount){
         let scoreDiff = finalCount - gameHistory[name]
         if(scoreDiff>0){
-            alert(`Sorry, you didn't beat your high score by ${scoreDiff}`);
+            alert (`That's Correct ${name}!  You did better in your last game by ${scoreDifference} fewer guesses.`);
         }else{
-            alert(`You tied your high score!`);
-    }
-    console.log(`Game History: ${JSON.stringify(gameHistory)}`)}
-    else if (gameHistory[name]>finalCount){
+             alert(`You tied your high score!`);
+        }
+    console.log(`Game History: ${JSON.stringify(gameHistory)}`)
+    }else if (gameHistory[name]>finalCount){
         let scoreDifference = gameHistory[name]-finalCount;
-        alert (`You beat your high score by ${scoreDifference}!`);
+        alert(`That's Correct ${name}! And you beat your previous attempt by ${scoreDiff} fewer guesses`);
         gameHistory[name]=finalCount;
         console.log(`Game History: ${JSON.stringify(gameHistory)}`)
         console.log(gameHistory)
     }
 }
-// playerName()
 play()
